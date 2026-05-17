9ccbb41afd627579a5d11dae23a806e7df8a62b7
Adarsh Mishra <adarshengineer145@gmail.com>
2026-05-17 14:49:41 +0530

    [Adarsh]: Updated environment.prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authServiceBaseUrl: 'http://107.22.130.57:8080/api/v1/auth',
-  paymentServiceBaseUrl: 'http://107.22.130.57:8080/api/payments'
+  authServiceBaseUrl: 'http://hireconnect.duckdns.org:8080/api/v1/auth',
+  paymentServiceBaseUrl: 'http://hireconnect.duckdns.org:8080/api/payments'
 };
