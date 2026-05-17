51b46eae84ed5ff060ec47ec910f44bde2a25992
Adarsh Mishra <adarshengineer145@gmail.com>
2026-05-17 15:09:29 +0530

    [Adarsh]: Updated environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // [Adarsh Mishra] : All API calls go through API Gateway on port 8080
-  authServiceBaseUrl: 'http://localhost:8080/api/v1/auth',
-  paymentServiceBaseUrl: 'http://localhost:8080/api/payments'
+  authServiceBaseUrl: 'http://hireconnect.duckdns.org:8080/api/v1/auth',
+  paymentServiceBaseUrl: 'http://hireconnect.duckdns.org:8080/api/payments'
 };
